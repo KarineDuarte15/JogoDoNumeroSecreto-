d263c469c46db8535521657203d345c0993caebd
KarineDuarte15 <151890625+KarineDuarte15@users.noreply.github.com>
2025-08-04 08:57:55 -0300

    alterações

---
+++ App.js
@@ -0,0 +1,62 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+        campo.innerHTML = texto;
+ }
+
+function exibirMensagemInicial() {
+     exibirTextoNaTela('h1', 'Jogo do número secreto');
+     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    
+}
+
+exibirMensagemInicial();
+
+                function verificarChute() {
+                    let chute = document.querySelector('input').value;
+
+                        // Verifica se o chute é um número entre 0 e 10
+                            if (isNaN(chute) || chute < 0 || chute > 10) {
+                                    alert('Só é permitido números de 0 a 10. Tente novamente.');
+                                            limparCampo();
+                                                    return; // Sai da função para não continuar a verificação
+                                                        }
+
+                                                            chute = parseInt(chute); // Garante que o chute será tratado como número inteiro
+
+                                                                if (chute === numeroSecreto) {
+                                                                        exibirTextoNaTela('h1', 'Acertou!');
+                                                                                let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+                                                                                        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+                                                                                                exibirTextoNaTela('p', mensagemTentativas);
+                                                                                                        document.getElementById('reiniciar').removeAttribute('disabled');
+                                                                                                            } else {
+                                                                                                                    if (chute > numeroSecreto) {
+                                                                                                                                exibirTextoNaTela('p', 'O número secreto é menor');
+                                                                                                                                        } else {
+                                                                                                                                                    exibirTextoNaTela('p', 'O número secreto é maior');
+                                                                                                                                                            }
+                                                                                                                                                                    tentativas++;
+                                                                                                                                                                            limparCampo();
+                                                                                                                                                                                }
+                                                                                                                                                                                }
+
+                                                                                                                                                                                function gerarNumeroAleatorio() {
+                                                                                                                                                                                    return parseInt(Math.random() * 10 + 1); // Gera de 1 a 10
+                                                                                                                                                                                    }
+
+                                                                                                                                                                                    function limparCampo() {
+                                                                                                                                                                                        let chute = document.querySelector('input');
+                                                                                                                                                                                            chute.value = '';
+                                                                                                                                                                                            }
+
+                                                                                                                                                                                            function reiniciarJogo() {
+                                                                                                                                                                                                numeroSecreto = gerarNumeroAleatorio();
+                                                                                                                                                                                                    limparCampo();
+                                                                                                                                                                                                        tentativas = 1;
+                                                                                                                                                                                                            exibirMensagemInicial();
+                                                                                                                                                                                                                document.getElementById('reiniciar').setAttribute('disabled', true);
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                                
